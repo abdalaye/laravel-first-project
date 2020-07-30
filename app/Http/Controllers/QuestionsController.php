919634fefcefd9a1750ba5857ceb149509c5c453
soumare <asoumare@afridev-group.com>
2020-07-30 12:47:45 +0000

    Comment DEbug mod

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,13 +14,13 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        \DB::enableQueryLog();
+        // \DB::enableQueryLog();
 
         $questions = Question::with('user')->latest()->paginate(5);
 
-        view('questions.index', compact('questions'))->render();
+        return view('questions.index', compact('questions'))->render();
 
-        dd(\DB::getQueryLog());
+        // dd(\DB::getQueryLog());
     }
 
     /**
